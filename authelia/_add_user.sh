19ae9e7bb2a2f11a0913e1fd0126d02e32a1a81d
James Curtin <jameswcurtin@gmail.com>
2020-09-20 12:06:36 -0400

    Standardize formatting & add linting

--- authelia/_add_user.sh
+++ authelia/_add_user.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ldapadd -x -c -D "cn=admin,dc=example,dc=org" -w $(cat /run/secrets/ldap-admin) -f /tmp/ldapadd.ldif -H ldap://authelia-openldap
+ldapadd -x -c -D "cn=admin,dc=example,dc=org" -w "$(cat /run/secrets/ldap-admin)" -f /tmp/ldapadd.ldif -H ldap://authelia-openldap
